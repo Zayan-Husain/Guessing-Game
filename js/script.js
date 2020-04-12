fe282c38c276316431127ec604cbc3288dd70f56
Zayan-Husain <63510104+Zayan-Husain@users.noreply.github.com>
2020-04-12 12:42:10 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
-	var numberToGuess = 4;
+	var numberToGuess = Math.floor(Math.random() * 100) + 1;
+	var numberOfGuesses = 0;
 	$(".container").on("click", ".submitNumber", function() {
 		var userGuess = $(".numberInput").val();
 		var textToChange = $(".higherOrLowerTxt");
 		if(userGuess == numberToGuess) {
-			textToChange.text("You won!");
+			textToChange.text("You won! It took you " + numberOfGuesses + " guesses");
 		}
 
 		if(userGuess < numberToGuess) {
 			textToChange.text("Higher.");
+			numberOfGuesses ++;
 		}
 
 		if(userGuess > numberToGuess) {
 			textToChange.text("Lower.");
+			numberOfGuesses ++;
 		}
 	})
 });
