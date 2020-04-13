b909abaca010d63e9b298556691e18ca861e50dc
Zayan-Husain <63510104+Zayan-Husain@users.noreply.github.com>
2020-04-13 13:13:34 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
+var numberToGuess = Math.floor(Math.random() * 100) + 1;
 $(document).ready(function(){
-	var numberToGuess = Math.floor(Math.random() * 100) + 1;
-	var numberOfGuesses = 0;
+	var numberOfGuesses = 1;
 	$(".container").on("click", ".submitNumber", function() {
 		var userGuess = $(".numberInput").val();
 		var textToChange = $(".higherOrLowerTxt");
 		if(userGuess == numberToGuess) {
-			textToChange.text("You won! It took you " + numberOfGuesses + " guesses.");
+			if(numberOfGuesses == 1) {
+				textToChange.text("You won! It took you " + numberOfGuesses + " guess.");
+			} else if(numberOfGuesses => 2) {
+				textToChange.text("You won! It took you " + numberOfGuesses + " guesses.");
+			}
 		}
 
 		if(userGuess < numberToGuess) {
@@ -18,4 +22,6 @@ $(document).ready(function(){
 			numberOfGuesses ++;
 		}
 	})
+	
+	
 });
